40df1afcb72ee1b80453610403d1c3a60fdab095
Heri Sandiyadi <herisandiyadi@gmail.com>
2025-07-02 15:15:23 +0700

    Initial Commit

---
+++ SubmissionTest/SubmissionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmissionTestApp.swift
+//  SubmissionTest
+//
+//  Created by Heri Sandiyadi on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SubmissionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SubmissionTestTests/SubmissionTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SubmissionTestTests.swift
+//  SubmissionTestTests
+//
+//  Created by Heri Sandiyadi on 02/07/25.
+//
+
+import Testing
+@testable import SubmissionTest
+
+struct SubmissionTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
